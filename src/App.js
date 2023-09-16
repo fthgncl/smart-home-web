eef4253f7f3d3fd88800a70ca18c9a0607296aa4
fthgncl <gencal.fatih61@gmail.com>
2023-09-16 05:02:00 +0300

    iki adet sayfa ve appbar eklendi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import AppBar from "./components/AppBar";
+import SignUpPage from './pages/SignUpPage';
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <div className="App">
-            Başlangıç
+            <AppBar/>
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
         </div>
     );
 }
