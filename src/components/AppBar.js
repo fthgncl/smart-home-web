bc6f08e34e9a0bb64a233d30cc0f5c1ba746e929
fthgncl <gencal.fatih61@gmail.com>
2023-09-19 23:10:59 +0300

    appBar Güncellendi

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,13 +3,38 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-export default function ButtonAppBar() {
+import {Account} from "../context/AccountContext";
+import {useDispatch} from 'react-redux';
+import {userDisconnected} from '../store/slices/accountSlice';
+
+export default function MenuAppBar() {
+
+    const dispatch = useDispatch();
+    const token = Account().accountProps.token;
+    const auth = token.active;
+
+    const handleLogOut = () => {
+        dispatch(userDisconnected());
+    }
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -17,14 +42,59 @@ export default function ButtonAppBar() {
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{ mr: 2 }}
+                        sx={{mr: 2}}
                     >
-                        <MenuIcon />
+                        <MenuIcon/>
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Typography onClick={()=>window.location.href = '/'}  variant="h6" component="div" sx={{flexGrow: 1, cursor: 'pointer'}}>
                         Smart Home
                     </Typography>
-                    <Button color="inherit">Giriş Yap</Button>
+                    {auth ? (
+                        <div>
+
+                            <IconButton
+                                sx={{borderRadius: 3}}
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <Typography variant="h6" component="div"
+                                            sx={{flexGrow: 2, marginRight: 2, fontSize: 15}}>
+                                    {token.data.name}
+                                </Typography>
+                                <AccountCircle/>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleLogOut}>Oturumu Kapat</MenuItem>
+                            </Menu>
+                        </div>
+                    ) : (
+                        <div>
+                            <IconButton onClick={()=>window.location.href = '/login'} sx={{borderRadius: 3}} size="large" color="inherit">
+                                <Typography sx={{flexGrow: 2, marginRight: 2, fontSize: 15}}>
+                                    Giriş Yap
+                                </Typography>
+                                <AccountCircle/>
+                            </IconButton>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
         </Box>
